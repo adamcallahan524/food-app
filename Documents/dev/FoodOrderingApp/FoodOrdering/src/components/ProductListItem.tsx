5a55584ef65efcd5e200168b3bda004c3b289daa
adamcallahan524 <adam.callahan33@gmail.com>
2024-12-25 16:51:30 -0500

    FlatList

--- Documents/dev/FoodOrderingApp/FoodOrdering/src/components/ProductListItem.tsx
+++ Documents/dev/FoodOrderingApp/FoodOrdering/src/components/ProductListItem.tsx
@@ -14,6 +14,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -28,6 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
 
   image: {
