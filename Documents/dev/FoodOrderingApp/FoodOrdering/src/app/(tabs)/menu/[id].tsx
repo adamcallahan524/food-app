4e4908d3a24a9d241982d6a5a0edb19b4cac23f0
adamcallahan524 <adam.callahan33@gmail.com>
2024-12-26 15:44:43 -0500

    shopping cart

--- Documents/dev/FoodOrderingApp/FoodOrdering/src/app/(tabs)/menu/[id].tsx
+++ Documents/dev/FoodOrderingApp/FoodOrdering/src/app/(tabs)/menu/[id].tsx
@@ -1,21 +1,30 @@
 import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React, { useState } from "react";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@assets/data/products";
 import { defaultPizzaImage } from "@/components/ProductListItem";
-import Button from "@components/Button";
+import Button from "@/components/Button";
+import { useCart } from "@/providers/CartProvider";
+import { PizzaSize } from "@/types";
 
-const sizes = ["S", "M", "L", "XL"];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
 
-  const [selectedSize, setSelectedSize] = useState("M");
+  const router = useRouter();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
   const product = products.find((p) => p.id.toString() === id);
 
   const addToCart = () => {
-    console.warn("Adding to cart, size: ", selectedSize);
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push("/cart");
   };
 
   if (!product) {
@@ -76,7 +85,7 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 18,
     fontWeight: "bold",
-    marginTop: "75",
+    marginTop: 75,
   },
 
   sizes: {
